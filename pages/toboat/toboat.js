71e1f9c41df1de6436a83e1ceffd6b45eddf8c15
likun_whu <1761834475@qq.com>
2019-05-01 15:01:17 +0800

    首页

--- pages/toboat/toboat.js
+++ pages/toboat/toboat.js
@@ -1,96 +1,83 @@
+// pages/toboat/toboat.js
+
+
+
 Page({
 
   /**
-
    * 页面的初始数据
-
-   * Linyufan.com
-
    */
-
   data: {
-    chooseBoat: true,//我要上船点击
-    chooseStatus: 0,//默认大船
-    animationData: {},//动画实例
+
+
     datalist: {},
     picture_production: [
       'https://xiaoyibang.top:8002/uploads/photo/4_2WZVla0.jpg',
       'https://xiaoyibang.top:8002/uploads/photo/9_9eMbYEf.jpg',
       'https://xiaoyibang.top:8002/uploads/photo/6_57uxCYM.jpg'
     ]
-
-
-
   },
-
-
 
   /**
-
    * 生命周期函数--监听页面加载
-
    */
-
-  onLoad: function (options) {
+  onLoad: function(options) {
+    console.log(options)
+    this.setData({
+      datalist: options
+    })
 
   },
 
-  onShow: function () {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
   },
-  choose: function (e) {
-    this.setData({
-      chooseStatus: e.currentTarget.dataset.index,
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
   },
-  showModal: function () {
-    console.log("真正")
-    // 显示遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
-    })
-    this.animation = animation
-    animation.translateY(300).step()
-    this.setData({
-      animationData: animation.export(),
-      chooseBoat: true
-    })
-    // setTimeout(function () {
-    //   animation.translateY(0).step()
-    //   this.setData({
-    //     animationData: animation.export()
-    //   })
-    // }.bind(this), 200)
 
-    animation.translateY(0).step()
-    this.setData({
-      animationData: animation.export()
-    })
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
   },
-  hideModal: function () {
-    // 隐藏遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
-    })
-    this.animation = animation
-    animation.translateY(300).step()
-    this.setData({
-      animationData: animation.export(),
-    })
-    setTimeout(function () {
-      animation.translateY(0).step()
-      this.setData({
-        animationData: animation.export(),
-        chooseBoat: false
-      })
-    }.bind(this), 200)
-  }
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
 
 
 
