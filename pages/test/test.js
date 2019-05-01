2c9635e24c7e80197877ed26a229176d97180bfb
liurongjie <1397608894@qq.com>
2019-05-01 14:33:32 +0800

    商品详情页

--- pages/test/test.js
+++ pages/test/test.js
@@ -9,18 +9,17 @@ Page({
    */
 
   data: {
+    chooseBoat:true,//我要上船点击
+    chooseStatus:0,//默认大船
+    animationData: {},//动画实例
+    datalist: {},
+    picture_production: [
+      'https://xiaoyibang.top:8002/uploads/photo/4_2WZVla0.jpg',
+      'https://xiaoyibang.top:8002/uploads/photo/9_9eMbYEf.jpg',
+      'https://xiaoyibang.top:8002/uploads/photo/6_57uxCYM.jpg'
+    ]
 
-    text: "这是一条测试公告，看看效果怎么样，2019年3月23日",
-
-    marqueePace: 1, //滚动速度
-
-    marqueeDistance: 0, //初始滚动距离
-
-    marquee_margin: 30,
-
-    size: 14,
-
-    interval: 20 // 时间间隔
+   
 
   },
 
@@ -38,51 +37,64 @@ Page({
 
   onShow: function() {
 
-    var that = this;
-
-    var length = that.data.text.length * that.data.size; //文字长度
-
-    var windowWidth = wx.getSystemInfoSync().windowWidth; // 屏幕宽度
-
-    //console.log(length,windowWidth);
-
-    that.setData({
-
-      length: length,
-
-      windowWidth: windowWidth
-
-    });
-
-    that.scrolltxt(); // 第一个字消失后立即从右边出现
+  },
+  choose:function(e){
+    this.setData({
+      chooseStatus: e.currentTarget.dataset.index,
+    })
 
   },
-
-
-
-  scrolltxt: function() {
-    var that = this;
-    var length = that.data.length; //滚动文字的宽度
-    var windowWidth = that.data.windowWidth; //屏幕宽度
-  
-    if (length > windowWidth) {
-      var interval = setInterval(function() {
-        var maxscrollwidth = length + that.data.marquee_margin; //滚动的最大宽度，文字宽度+间距，如果需要一行文字滚完后再显示第二行可以修改marquee_margin值等于windowWidth即可
-        var crentleft = that.data.marqueeDistance;
-        console.log(crentleft, maxscrollwidth)
-          that.setData({
-
-            marqueeDistance: crentleft + that.data.marqueePace
-          })
-      }, that.data.interval);
-
-    } else {
-      that.setData({
-        marquee_margin: "1000"
-      }); //只显示一条不滚动右边间距加大，防止重复显示
-
-    }
-
+  showModal: function () {
+    console.log("真正")
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      chooseBoat: true
+    })
+    // setTimeout(function () {
+    //   animation.translateY(0).step()
+    //   this.setData({
+    //     animationData: animation.export()
+    //   })
+    // }.bind(this), 200)
+    
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+   
+  },
+  hideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        chooseBoat: false
+      })
+    }.bind(this), 200)
   }
+
+
+
+
+  
 
 })
