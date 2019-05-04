7bc8c931c2e6349a0626906a098b9fc3f5a9db62
likun_whu <2016302590230@whu.edu.cn>
2019-05-04 17:01:19 +0800

    selected

--- pages/home/home.js
+++ pages/home/home.js
@@ -66,7 +66,12 @@ Page({
     winHeight: "", //窗口高度
     currentTab: 0, //预设当前项的值
     scrollLeft: 0, //tab标题的滚动条位置
-    
+
+
+    orderName_list:['按综合','按价格','按距离'],
+    orderName:'按综合',
+    showModal: false,
+
 
   },
   onslidechangeend: function (e) {
@@ -230,6 +235,37 @@ Page({
     }
   },
 
+  showDialogBtn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+  /**
+   * 弹出框蒙层截断touchmove事件
+   */
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+  /**
+   * 对话框确认按钮点击事件
+   */
+  onConfirm: function () {
+    this.hideModal();
+  }
 
+ 
 
 })
